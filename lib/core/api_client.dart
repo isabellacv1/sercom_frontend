06d3502fa7a50d05279d745a0eb1aa7340f6f90e
isabellacv <isabellacuervo4@gmail.com>
2026-04-22 12:23:47 -0500

    fix: delete .env

--- lib/core/api_client.dart
+++ lib/core/api_client.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiClient {
   static final ApiClient _instance = ApiClient._internal();
@@ -20,17 +19,16 @@ class ApiClient {
 
     dio.interceptors.add(
       InterceptorsWrapper(
-        onRequest: (options, handler) async {
-          final prefs = await SharedPreferences.getInstance();
-          final secretKey = dotenv.env['SUPABASE_SECRET_KEY'] ?? '';
-          final token = prefs.getString('token') ?? secretKey;
-          
-          if (token.isNotEmpty) {
-            options.headers['Authorization'] = 'Bearer $token';
-          }
+      onRequest: (options, handler) async {
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('token') ?? '';
 
-          handler.next(options);
-        },
+  if (token.isNotEmpty) {
+    options.headers['Authorization'] = 'Bearer $token';
+  }
+
+  handler.next(options);
+},
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
